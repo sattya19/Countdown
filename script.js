81cf72503d4baecd01f053308c3974188268a5b7
Satya Sharma <sattyasharma195@gmail.com>
2023-03-04 23:09:46 +0530

    edited JS

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const countdown =() =>{
-    const countDate = new Date ('May 19,2023 00:00:00').getTime();
+const countdown = () => {
+    const date = new Date('05 19,2023 00:00:00');
+    const countDate = date.getTime();
+    
     const now = new Date().getTime();
     const gap  = countDate - now;
     
@@ -7,23 +9,29 @@ const countdown =() =>{
     const minutes = seconds*60;
     const hours = minutes* 60;
     const days= hours * 24;
-   
+    const d = new Date();
+    let month = d.getMonth();
 
-
+    let monthsRemaining = date.getMonth() - month;
+     
+    const daysInMonth= "31 28 31 30 31 30 31 31 30 31 30 31".split(" ");
+    const daysInCurrentMonth = daysInMonth[month];
+    const todaysDate = d.getDate();
+    const remainingDays = daysInCurrentMonth - todaysDate;
 
     const textDays = Math.floor(gap / days);
     const textHours = Math.floor((gap % days)/hours);
     const textMinutes = Math.floor((gap % hours )/minutes);
     const textSeconds = Math.floor((gap % minutes)/seconds);
-
     
 
-
-    document.querySelector('.days').innerText= textDays;
+    document.querySelector('.days').innerText= remainingDays;
  
     document.querySelector('.hours').innerText= textHours;
+
     document.querySelector('.minutes').innerText= textMinutes;
     document.querySelector('.seconds').innerText= textSeconds;
+    document.querySelector('.months').innerText= monthsRemaining;
 
 
 }
