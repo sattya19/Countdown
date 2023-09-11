5435c3368e99822e60cd3e99d908c19899898671
Satya Sharma <83288606+sattya19@users.noreply.github.com>
2023-09-11 14:06:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,53 @@
-const randomBackgroundAPI = "https://picsum.photos/1920/1080/";
-
 const backgroundImage = document.getElementById("backgroundImage");
+
+// Define the randomBackgroundAPI variable
+const randomBackgroundAPI = "your_random_background_api_url_here";
+
+// Load the background image
 backgroundImage.addEventListener("load", () => {
-  backgroundImage.style.opacity = 1;
+    backgroundImage.style.opacity = 1;
 });
 
-fetch(randomBackgroundAPI).then((response) => {
-  const backgroundImageEle = document.getElementById("backgroundImage");
-  backgroundImageEle.setAttribute("src", `${response.url}`);
-});
-
+fetch(randomBackgroundAPI)
+    .then((response) => response.json()) // Assuming the response is JSON
+    .then((data) => {
+        const backgroundImageEle = document.getElementById("backgroundImage");
+        backgroundImageEle.setAttribute("src", data.url);
+    });
 
 const countdown = () => {
-  const date = new Date("04 05,2024 00:00:00");
-  const countDate = date.getTime();
+    const currentDate = new Date();
+    const targetDate = new Date("April 5, 2024 00:00:00");
 
-  const now = new Date().getTime();
-  const gap = countDate - now;
+    const gap = targetDate - currentDate;
 
-  const seconds = 1000;
-  const minutes = seconds * 60;
-  const hours = minutes * 60;
-  const days = hours * 24;
-  const d = new Date();
-  let month = d.getMonth();
+    const seconds = 1000;
+    const minutes = seconds * 60;
+    const hours = minutes * 60;
+    const days = hours * 24;
 
-  let monthsRemaining = date.getMonth() - month;
+    const textHours = String(Math.floor((gap % days) / hours)).padStart(2, "0");
+    const textMinutes = String(Math.floor((gap % hours) / minutes)).padStart(2, "0");
+    const textSeconds = String(Math.floor((gap % minutes) / seconds)).padStart(2, "0");
 
-  const daysInMonth = "31 28 31 30 31 30 31 31 30 31 30 31".split(" ");
-  const daysInCurrentMonth = daysInMonth[month];
-  const todaysDate = d.getDate();
+    document.querySelector(".hours").innerText = textHours;
+    document.querySelector(".minutes").innerText = textMinutes;
+    document.querySelector(".seconds").innerText = textSeconds;
 
-  let remainingDays;
-  if(monthsRemaining>0){
-    remainingDays= daysInCurrentMonth- todaysDate;
-  }
-  else{
-    remainingDays = date.getDate() - todaysDate;
-  }
+    // Calculate months remaining
+    let monthsRemaining = targetDate.getMonth() - currentDate.getMonth();
+    if (monthsRemaining < 0) {
+        monthsRemaining += 12; // Adjust for negative months
+    }
+    document.querySelector(".months").innerText = monthsRemaining;
 
-
-  const textHours = Math.floor((gap % days) / hours);
-  const textMinutes = Math.floor((gap % hours) / minutes);
-  const textSeconds = Math.floor((gap % minutes) / seconds);
- 
-
-  document.querySelector(".days").innerText = remainingDays;
-
-  document.querySelector(".hours").innerText = textHours;
-
-  document.querySelector(".minutes").innerText = textMinutes;
-  document.querySelector(".seconds").innerText = textSeconds;
-  document.querySelector(".months").innerText = monthsRemaining;
+    // Calculate remaining days
+    const remainingDays = Math.floor(gap / days);
+    document.querySelector(".days").innerText = remainingDays;
 };
+
+// Call the countdown function once to initialize
+countdown();
+
+// Update the countdown every 1000ms (1 second)
 setInterval(countdown, 1000);
